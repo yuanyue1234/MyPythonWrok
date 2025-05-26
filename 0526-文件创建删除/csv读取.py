757ea10ac265c82d79f74cc2762485960df2a940
asunny <e@asunny.top>
2025-05-26 10:02:25 +0800

    2025-05-26 09:56:34

--- 0526-文件创建删除/csv读取.py
+++ 0526-文件创建删除/csv读取.py
@@ -1,5 +1,8 @@
 import csv
-f =  open('test2.csv','r',encoding='utf-8')
+f =  open('text.csv','r',encoding='utf-8')
 csv_reader = csv.reader(f) # 创建读取器对象
 for line in csv_reader:
     print(line)
+csvfile = open('test2.csv','w',encoding='utf-8')
+csvwriter = csv.writer(csvfile)
+csvwriter.writerow(['测试','测试','测试3'])
